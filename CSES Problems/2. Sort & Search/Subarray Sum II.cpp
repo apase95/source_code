491c1fc90a9ca5645a3318cf3a334fab43a49dd6
apase95 <noobyhandsome@gmail.com>
2024-09-13 11:00:49 +0700

    add commit

---
+++ CSES Problems/2. Sort & Search/Subarray Sum II.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pll pair<ll,ll>
+#define F first
+#define S second
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define eb emplace_back
+#define ins insert
+#define ers erase
+#define gcd __gcd
+#define LB lower_bound
+#define UB upper_bound
+
+#define FOR(i,a,b) for(ll i=(a);i<=(b);++i)
+#define FOD(i,a,b) for(ll i=(a);i>=(b);--i)
+#define bitUp(i,n) for(;i<=n;i+=i&-i)
+#define bitGet(i) for(;i>0;i-=i&-i)
+
+#define nooby_speedrun ios_base::sync_with_stdio(0);cin.tie(0),cout.tie(0);
+#define checkIO if(fopen("TEST.INP","r")){freopen("TEST.INP","r",stdin);freopen("TEST.OUT","w",stdout);}
+
+const ll MOD=1e9+7;
+const ll NMAX=2e5+5;
+const ll N=5e3+5;
+const ll INF=1e9;
+
+
+/*----------------------------------------------[ YOU GAY! ]-------------------------------------------*/
+
+
+ll i,j,n,m,k;
+ll a[NMAX];
+map<ll,ll> Hash;
+
+
+int main()
+{
+    nooby_speedrun
+    checkIO
+
+    cin >> n >> k;
+    FOR(i,1,n) cin >> a[i];
+    
+    ll sum=0,res=0; Hash[0]=1;
+    
+    FOR(i,1,n)
+    {
+        sum+=a[i];
+        res+=Hash[sum-k];
+        ++Hash[sum];
+    }
+
+    return cout << res,0;
+}
+
+
+/*----------------------------------------------------------------------------------*
+*                                                   _     _                         *
+*      |------------------------|              |\__/,|   ( )                        *
+*      | author: noobyhandsome  |            _.|o o  |_   ) )               <3      *
+*      |------------------------|            (((-^-(((------)                       *
+*                                                                                   *
+*----------------------------------------------------------------------------------*/
---
+++ CSES Problems/2. Sort & Search/Sum of Three Values.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pll pair<ll,ll>
+#define F first
+#define S second
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define eb emplace_back
+#define ins insert
+#define ers erase
+#define gcd __gcd
+#define LB lower_bound
+#define UB upper_bound
+
+#define FOR(i,a,b) for(ll i=(a);i<=(b);++i)
+#define FOD(i,a,b) for(ll i=(a);i>=(b);--i)
+#define bitUp(i,n) for(;i<=n;i+=i&-i)
+#define bitGet(i) for(;i>0;i-=i&-i)
+
+#define nooby_speedrun ios_base::sync_with_stdio(0);cin.tie(0),cout.tie(0);
+#define checkIO if(fopen("TEST.INP","r")){freopen("TEST.INP","r",stdin);freopen("TEST.OUT","w",stdout);}
+
+const ll MOD=1e9+7;
+const ll NMAX=2e5+5;
+const ll N=5e3+5;
+const ll INF=1e9;
+
+
+/*----------------------------------------------[ YOU GAY! ]-------------------------------------------*/
+
+
+ll i,j,n,m,k;
+ll sum;
+pll a[NMAX];
+
+
+ll cmp(pll a,pll b) {return a.F<b.F;}
+
+ll BinarySearch(ll L,ll R,ll val)
+{
+    while(L<=R)
+    {
+        ll M=(L+R)>>1;
+        if (a[M].F==val) return a[M].S;
+        else if (a[M].F>val) return R=M-1;
+        else L=M+1;
+    }
+    return -1;
+}
+
+
+int main()
+{
+    nooby_speedrun
+    checkIO
+
+    cin >> n >> sum;
+    FOR(i,1,n) {cin >> a[i].F; a[i].S=i;}
+    sort(a+1,a+n+1,cmp);
+
+    FOR(i,1,n-2)
+    {
+        j=i+1; k=n;
+        while(j<=k)
+        {
+            ll temp=a[i].F+a[j].F+a[k].F;
+            if (temp==sum && a[i].S!=a[j].S && a[j].S!=a[k].S && a[k].S!=a[i].S) 
+                return cout << a[i].S << " " << a[j].S << " " << a[k].S,0;
+            else if (temp>sum) --k;
+            else ++j;
+        }
+    }
+
+    return cout << "IMPOSSIBLE",0;
+}
+
+
+/*----------------------------------------------------------------------------------*
+*                                                   _     _                         *
+*      |------------------------|              |\__/,|   ( )                        *
+*      | author: noobyhandsome  |            _.|o o  |_   ) )               <3      *
+*      |------------------------|            (((-^-(((------)                       *
+*                                                                                   *
+*----------------------------------------------------------------------------------*/
